cd2eb1583878baad306bf716b0dc04b87601bebe
Vinicius Passoni <vinicius@gempe.dev>
2022-01-15 22:31:12 -0300

    add graphql-yoga

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const { GraphQLServer } = require('graphql-yoga');
+const path = require('path');
+const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+})
+
+const server = new GraphQLServer({
+    typeDefs: path.resolve(__dirname, 'schema.graphql'),
+    resolvers,
+});
+
+server.start(() => console.log('Server is running on localhost:4000'));
